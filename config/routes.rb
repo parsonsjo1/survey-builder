9ae6cd78dd9dc1796bdb7ba94ee91eca4bb7dee3
parsonsjo1 <jopa.d.parsons@gmail.com>
2017-02-19 22:46:27 -0700

    Updated routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,74 +1,7 @@
 Rails.application.routes.draw do
-  get 'answers/index'
 
-  get 'answers/new'
-
-  get 'answers/create'
-
-  get 'answers/show'
-
-  get 'answers/edit'
-
-  get 'answers/update'
-
-  get 'answers/destroy'
-
-  get 'responses/index'
-
-  get 'responses/new'
-
-  get 'responses/create'
-
-  get 'responses/show'
-
-  get 'responses/edit'
-
-  get 'responses/update'
-
-  get 'responses/destroy'
-
-  get 'choices/index'
-
-  get 'choices/new'
-
-  get 'choices/create'
-
-  get 'choices/show'
-
-  get 'choices/edit'
-
-  get 'choices/update'
-
-  get 'choices/destroy'
-
-  get 'questions/index'
-
-  get 'questions/new'
-
-  get 'questions/create'
-
-  get 'questions/show'
-
-  get 'questions/edit'
-
-  get 'questions/update'
-
-  get 'questions/destroy'
-
-  get 'surveys/index'
-
-  get 'surveys/new'
-
-  get 'surveys/create'
-
-  get 'surveys/show'
-
-  get 'surveys/edit'
-
-  get 'surveys/update'
-
-  get 'surveys/destroy'
-
+  root 'surveys#new'
+  
   get 'users/new'
 
   get 'users/create'
@@ -80,6 +13,12 @@ Rails.application.routes.draw do
   get 'users/update'
 
   get 'users/destroy'
+
+  resources :surveys
+  resources :questions
+  resources :choices
+  resources :responses
+  resources :answers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
